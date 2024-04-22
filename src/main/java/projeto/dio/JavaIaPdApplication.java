2aa752ea2d78a6f88f9b286cbc1dd208da5c8a69
MarcoTech1234 <marcoahonorio2@gmail.com>
2024-04-22 14:46:50 -0300

    Atualizar repositório com as alterações do GitHub

---
+++ src/main/java/projeto/dio/JavaIaPdApplication.java
@@ -0,0 +1,13 @@
+package projeto.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIaPdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIaPdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/dio/JavaIaPdApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIaPdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
